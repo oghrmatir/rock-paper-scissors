4525b84831c0298a8b358b03b7c26132f403ae61
Argho Maitra <207898299+oghrmatir@users.noreply.github.com>
2025-04-26 22:50:43 +0530

    Add logic to retrieve the computer choice
    Function makes use of the getRandomIntInclusive(min, max) function in
    order to return one of the desired strings.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+function getComputerChoice() {
+  let randomNumber = getRandomIntInclusive(0, 2);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else if (randomNumber === 2) {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice())
