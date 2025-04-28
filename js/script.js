9c4702327ac435590a45831e3d69d50e022c9b8c
Argho Maitra <207898299+oghrmatir@users.noreply.github.com>
2025-04-28 13:21:18 +0530

    Apply the logic for resetting the game with audio

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    result.textContent = "It's a draw!";
+    result.textContent = "What a bummer! It's a draw!";
   } else if (humanChoice === "Rock") {
     if (computerChoice === "Paper") {
       result.textContent = `You lose! ${computerChoice} beats ${humanChoice}.`;
@@ -67,18 +67,33 @@ function playRound(humanChoice, computerChoice) {
 
 function declareWinner(humanScore, computerScore) {
   if (humanScore === 5) {
-    winner.textContent = "Human won!";
+    winner.textContent = "Result: You have finally won!";
   } else if (computerScore === 5) {
-    winner.textContent = "Computer won!";
+    winner.textContent = "Result: The Computer has won!";
   }
+}
+
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  result.textContent = "";
+  score.textContent = "";
+  winner.textContent = "";
 }
 
 const buttons = document.querySelectorAll(".btn");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
+    if (button.textContent === "Reset") {
+      restartAudio.play();
+      resetGame();
+      return;
+    }
+
+    selectAudio.play();
+
     if (humanScore === 5 || computerScore === 5) {
-      declareWinner(humanScore, computerScore);
       return;
     }
 
@@ -87,10 +102,15 @@ buttons.forEach((button) => {
     
     playRound(humanSelection, computerSelection);
 
-    score.textContent = `Computer: ${computerScore}, Human: ${humanScore}`;
+    score.textContent = `Score: Computer: ${computerScore} | Human: ${humanScore}`;
+
+    declareWinner(humanScore, computerScore);
   });
 });
 
 const result = document.querySelector(".result");
 const score = document.querySelector(".score");
 const winner = document.querySelector(".winner");
+
+const selectAudio = document.querySelector(".select");
+const restartAudio = document.querySelector(".restart");
